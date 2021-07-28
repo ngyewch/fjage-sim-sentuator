8c15f6c2ef1eb0f434336a41a1237bb7f9c8b2e8
Nick Ng <ngyewch@gmail.com>
2021-07-28 17:26:35 +0800

    chore: added publishing support

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,23 @@
 plugins {
     `java-library`
+    `maven-publish`
+    signing
     id("ca.cutterslade.analyze") version "1.7.1"
     id("com.asarkar.gradle.build-time-tracker") version "3.0.1"
     id("com.github.ben-manes.versions") version "0.39.0"
 }
 
+project.group = "com.github.ngyewch.fjage-sim-sentuator"
+project.version = "0.1.0"
+
+val isReleaseVersion = !(project.version as String).endsWith("SNAPSHOT")
+
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
+
+    withJavadocJar()
+    withSourcesJar()
 }
 
 dependencies {
@@ -24,28 +34,88 @@ repositories {
     mavenCentral()
 }
 
-tasks.withType<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask> {
-    gradleReleaseChannel = "current"
+tasks {
+    withType<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask> {
+        gradleReleaseChannel = "current"
 
-    fun isNonStable(version: String): Boolean {
-        val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
-        val regex = "^[0-9,.v-]+(-r)?$".toRegex()
-        val isStable = stableKeyword || regex.matches(version)
-        return isStable.not()
+        fun isNonStable(version: String): Boolean {
+            val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
+            val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+            val isStable = stableKeyword || regex.matches(version)
+            return isStable.not()
+        }
+
+        rejectVersionIf {
+            isNonStable(candidate.version) && !isNonStable(currentVersion)
+        }
     }
 
-    rejectVersionIf {
-        isNonStable(candidate.version) && !isNonStable(currentVersion)
+    test {
+        useJUnitPlatform()
+        testLogging {
+            events = setOf(
+                org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED,
+                org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED,
+                org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED
+            )
+        }
+    }
+
+    withType(Sign::class) {
+        onlyIf { isReleaseVersion }
     }
 }
 
-tasks.test {
-    useJUnitPlatform()
-    testLogging {
-        events = setOf(
-            org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED,
-            org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED,
-            org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED
-        )
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group as String
+            artifactId = project.name
+            version = project.version as String
+
+            from(components["java"])
+
+            pom {
+                name.set("fjage-sim-sentuator")
+                description.set("fjage simulated sentuators.")
+                url.set("https://github.com/ngyewch/fjage-sim-sentuator")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://github.com/ngyewch/fjage-sim-sentuator/blob/main/LICENSE")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git@github.com:ngyewch/fjage-sim-sentuator.git")
+                    developerConnection.set("scm:git:git@github.com:ngyewch/fjage-sim-sentuator.git")
+                    url.set("https://github.com/ngyewch/fjage-sim-sentuator")
+                }
+                developers {
+                    developer {
+                        id.set("ngyewch")
+                        name.set("Nick Ng")
+                    }
+                }
+            }
+        }
     }
+    repositories {
+        maven {
+            if (isReleaseVersion) {
+                setUrl("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            } else {
+                setUrl("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+            }
+            credentials {
+                val ossrhUsername: String? by project
+                val ossrhPassword: String? by project
+                username = ossrhUsername
+                password = ossrhPassword
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["maven"])
 }
